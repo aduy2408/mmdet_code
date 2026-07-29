1f42874fda2f772e37dcb2aa2e2d6e82663c6e32
Duy <lengocduy169@gmail.com>
2026-07-29 19:41:51 +0700

    chore: add LMSCE optimization controls

---
+++ mmdetection/configs/hit/fcos_r50-caffe_fpn_lmsce-ring-zerolr5_30e_levir-ship-768.py
@@ -0,0 +1,10 @@
+_base_ = './fcos_r50-caffe_fpn_lmsce-ring_30e_levir-ship-768.py'
+
+optim_wrapper = dict(
+    paramwise_cfg=dict(
+        bias_lr_mult=2.,
+        bias_decay_mult=0.,
+        custom_keys={
+            'neck.lmsce_modules.0.transform.3': dict(lr_mult=5.)
+        }))
+work_dir = 'work_dirs/lmsce_opt_control/ring_lr5'
