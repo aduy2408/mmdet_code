21411318c5303cf033760cc19ab3305a5c7ce7fc
Jcode <jcode@users.noreply.github.com>
2026-09-19 09:45:34 +0700

    Keep SR-TOD AMP ROIAlign geometry in float32

--- SR-TOD/mmdet/models/roi_heads/roi_extractors/single_level_roi_extractor.py
+++ SR-TOD/mmdet/models/roi_heads/roi_extractors/single_level_roi_extractor.py
@@ -106,8 +106,12 @@ class SingleRoIExtractor(BaseRoIExtractor):
                 # Under AMP, FPN levels can have different dtypes. MMCV's
                 # ROIAlign requires RoIs to match the feature dtype for the
                 # selected level, not only feats[0].
-                rois_ = rois[inds].type_as(feats[i])
-                roi_feats_t = self.roi_layers[i](feats[i], rois_)
+                rois_ = rois[inds].float()
+                # MMCV's ROIAlign extension is not reliable with the mixed
+                # Half/Float tensors produced by this AMP path. Keep this
+                # small geometry op in float32 while the backbone/FPN stay
+                # autocast-enabled.
+                roi_feats_t = self.roi_layers[i](feats[i].float(), rois_)
                 roi_feats[inds] = roi_feats_t.to(roi_feats.dtype)
             else:
                 # Sometimes some pyramid levels will not be used for RoI
